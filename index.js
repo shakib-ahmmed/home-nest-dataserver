591eeb4b2edd3e40081ea49b0943fe99d2315ffe
SHAKIB AHMMED <shakibahmmed1@gmail.com>
2025-11-10 21:46:23 +0600

    server port updated to 5000

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.ovreryk.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://homenestDBUSer:oXM0V2Eg9Hwdc010@cluster0.ovreryk.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -21,31 +21,28 @@ const client = new MongoClient(uri, {
     }
 });
 
-
 app.get('/', (req, res) => {
-    res.send('smart server is running')
-})
+    res.send('HomeNest API is running');
+
+
+});
+
+
 
 async function run() {
     try {
         await client.connect();
-        const db = client.db('homenestDBUSers')
-        const propertiesCollection = db.collection('properties')
+
+        const db = client.db('homenestDBUser')
+        const propertiesCollection = db.collection('properties');
+
 
         app.get('/properties', async (req, res) => {
 
             const result = await propertiesCollection.find().toArray()
 
-            console.log(result)
 
-
-
-
-
-
-
-
-            res.send('result')
+            res.send(result)
         })
 
 
@@ -54,12 +51,14 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
+
+
     }
 }
 
 
-run().catch(console.dr)
+run().catch(console.error);
 
 app.listen(port, () => {
-    console.log(`smart server is running on port: ${port}`)
+    console.log(`HomeNest server is running on port: ${port}`)
 })
